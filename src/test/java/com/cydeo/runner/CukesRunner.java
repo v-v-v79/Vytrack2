00cf4d8b80635d8ddacc8b98b6ce6d57e6aba8df
v-v-v79 <vivedesh@gmail.com>
2022-11-11 23:02:25 -0800

    jenkins and junit plugins are added to CukesRunner

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -8,7 +8,9 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
+                "json:target/cucumber.json",
                 "html:target/cucumber-report.html",
+                "junit:target/junit/junit-report.xml",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
